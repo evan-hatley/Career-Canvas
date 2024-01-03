98b94514a99106feedd876cff5b9cca55842a661
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2024-01-02 19:44:45 -0600

    Chart setup and db
    Also added package and package lock with relevant dependencies

---
+++ chart.js
@@ -0,0 +1,16 @@
+// Setting up for doughnut chart data
+
+// const data = {
+//     labels: ['Applied', 'Interviewed', 'Offer', 'Declined'],
+//     datasets: [{
+//       label: 'Job Application Status',
+//       data: [10, 5, 2, 3],
+//       borderWidth: 1
+//     }]
+//   };
+
+// const config = {
+//     type: 'doughnut',
+//     data: data,
+    
+// }
